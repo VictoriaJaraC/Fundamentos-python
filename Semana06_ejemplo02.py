953caa2d3e44ce4ca4b09c6829a14c4721bd05df
VictoriaJaraC <VICTORIA.JARACASTRO@UCR.AC.CR>
2026-09-14 16:34:39 +0000

    Version07

---
+++ Semana06_ejemplo02.py
@@ -0,0 +1,40 @@
+from eii_utils import limpiar_consola, leer_flotante, leer_entero, leer_booleano
+
+salario_bruto: float = 0
+salario_neto: float = 0
+renta: float = 0
+hijos: int = 0
+tiene_conyuge: bool = True 
+creditos: float = 0 
+
+limpiar_consola()
+salario_bruto = leer_flotante("Digite el salario")
+tiene_conyuge = leer_booleano("tiene conyuge")
+hijos = leer_entero("Digite la cantidad de hijos")
+
+if salario_bruto <= 918_000:
+    renta = 0 
+elif salario_bruto <= 1_347_000:
+    renta = (salario_bruto - 918_000) * 0.1
+elif salario_bruto <= 2_364_000:
+    renta = 0 + 42_900 + (salario_bruto - 1_347_000)*0.15
+elif salario_bruto <= 4_727_000:
+    renta = 0 + 42_900 + 152_550 +(salario_bruto - 2_364_000)* 0.20
+else:
+    renta = 0 + 42_900 + 152_550 + 472_600 + (salario_bruto - 4_727_000)*0.25
+
+if tiene_conyuge:
+    creditos = 2590
+else:
+    credito = 0
+
+creditos = creditos + hijos * 1720
+
+if renta >= creditos:
+    renta = renta - creditos
+else:
+    renta= 0 
+
+salario_neto = salario_bruto - renta 
+
+print(salario_neto)
